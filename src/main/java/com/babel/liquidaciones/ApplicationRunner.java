eaed2afc2b521f9dcad4faaaab33c700f424c5ce
Andrea Auñón Antúnez <andrea.aunon@babelgroup.com>
2024-03-06 11:08:45 +0100

    Base menu creado

--- src/main/java/com/babel/liquidaciones/ApplicationRunner.java
+++ src/main/java/com/babel/liquidaciones/ApplicationRunner.java
@@ -1,12 +1,15 @@
 package com.babel.liquidaciones;
 
 import com.babel.liquidaciones.configuration.AppConfig;
+import com.babel.liquidaciones.view.Menu;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
+        Menu menu = new Menu();
+        menu.startSession();
     }
 }
