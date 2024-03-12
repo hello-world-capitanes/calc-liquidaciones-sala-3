c0abcd9f338f93ff745ba03f3cb0c3e3b3fb479c
Guillermo Dylan Carvajal Aza <guillermo.carvajal@babelgroup.com>
2024-03-12 13:54:44 +0100

    Base para calculo general

--- src/main/java/com/babel/liquidaciones/services/CalculoGeneral.java
+++ src/main/java/com/babel/liquidaciones/services/CalculoGeneral.java
@@ -1,17 +1,46 @@
 package com.babel.liquidaciones.services;
 
-import com.babel.liquidaciones.model.Daño;
-import com.babel.liquidaciones.model.Poliza;
-import com.babel.liquidaciones.model.Siniestro;
-import com.babel.liquidaciones.model.Warranty;
+import com.babel.liquidaciones.model.*;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CalculoGeneral implements ICalculoGeneral {
+
+    private ICalculoValService calculoValService;
+
+    public CalculoGeneral(ICalculoValService calculoValService) {
+        this.calculoValService = calculoValService;
+    }
+
     @Override
     public void calcular(Siniestro siniestro) {
         //Se comprueba si la garantía está excluida
         //a. En caso de exclusión la liquidación es 0.
+        double liquidación = 0;
         for (Daño daño : siniestro.getDaños()) {
+            for (ProductWarranty productWarranty : daño.getProducto().getProductWarranties()) {
+                Warranty warranty = productWarranty.getWarranty();
+                if (!productWarranty.isExcluded()) {
+                    liquidación += productWarranty.getCapitalInsured();
+                }
+                //2.	Se calcula el importe a liquidar en función del tipo de valoración
+                if (productWarranty.getPaymentType().equals(PaymentType.PRIMER_RIESGO)) {
+                    liquidación += this.calculoValService.calcularPrimerRiesgo(siniestro);
+                } else if (productWarranty.getPaymentType().equals(PaymentType.REPOSICION_NUEVO)) {
+                    liquidación += this.calculoValService.calcularReposicion(siniestro);
+                } else {
+                    liquidación += this.calculoValService.calcularValorReal(siniestro);
+                }
+                //3.	Se calcula el importe a liquidar en función del tipo de aseguramiento (tomando como entrada el importe calculado en punto 2)
+                if (warranty.getWarrantyType().equals(WarrantyType.BUILDING)) {
+
+                } else if (warranty.getWarrantyType().equals(WarrantyType.CONTENT)) {
+
+                }
+                //4.	Se aplica la regla de infraseguro
+            }
 
         }
+
     }
 }
